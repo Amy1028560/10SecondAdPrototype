b889d3eb542d0832da00fb104a4e46056c0b8e88
CamStar28 <129808323+CamStar28@users.noreply.github.com>
2023-09-28 12:48:29 +1000

    Title Screen Added + Live System Partially Implemented
    Clicking on ads doesn't properly remove lives yet, but the system is in place

--- Assets/AdClose.cs
+++ Assets/AdClose.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,15 +7,36 @@ public class AdClose : MonoBehaviour
     public GameObject closeButton;
     public GameObject adContent;
 
+    public int playerLives; 
+
     void Start()
     {
+        playerLives = 3; 
+        
         closeButton.GetComponent<Button>().onClick.AddListener(CloseAd);
-      //  adContent.GetComponent<Button>().onClick.AddListener(GameOver);
+        //adContent.GetComponent<Button>().onClick.AddListener(GameOver);
+    }
+
+    private void Update()
+    {
+        if (playerLives == 0)
+        {
+            //add scene reload code here
+        }
     }
 
     void CloseAd()
     {
         adContent.SetActive(false);
+    }
+
+    public void OnMouseOver()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            playerLives--;
+            Debug.Log("wow cam, you fuckin' did it, dummy"); 
+        }
     }
 
     void GameOver()
